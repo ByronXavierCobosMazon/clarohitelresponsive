d57d00774235a01d776d96f90b0f9f94a191e4e1
ByronXavierCobosMazon <byronxaviercobos@gmail.com>
2023-01-17 16:53:13 -0500

    bctbt

---
+++ app.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('#form1');
+const btc = document.querySelector('#btc');
+
+function validar() {
+    const desabilitar = false;
+
+    if(form.NOMBRE.value===''){
+        desabilitar = true;
+    }
+    if(form.TELÉFONO.value===''){
+        desabilitar = true;
+    }
+    if (desabilitar ===true){
+        btc.desabled = true;
+    }else{
+        btc.desibled = false;
+    }
+}
+
+form.addEventListener("keyup", validar)
