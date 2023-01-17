e5669d294066d12429b115e1936a8fdc27550c91
ByronXavierCobosMazon <byronxaviercobos@gmail.com>
2023-01-17 17:10:01 -0500

    btcvalid

--- app.js
+++ app.js
@@ -18,3 +18,24 @@ function validar() {
 }
 
 form.addEventListener("keyup", validar)
+
+const form = document.querySelector('#form2');
+const btc = document.querySelector('#btc');
+
+function validar() {
+    const desabilitar = false;
+
+    if(form.NOMBRE.value===''){
+        desabilitar = true;
+    }
+    if(form.TELÉFONO.value===''){
+        desabilitar = true;
+    }
+    if (desabilitar ===true){
+        btc.desabled = true;
+    }else{
+        btc.desibled = false;
+    }
+}
+
+form.addEventListener("keyup", validar)
