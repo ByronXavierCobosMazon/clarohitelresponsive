86bf51cb0324606273e0c8d61359035f6f3d86c7
ByronXavierCobosMazon <byronxaviercobos@gmail.com>
2023-01-17 16:05:40 -0500

    submit

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-var nombre = document.getElementById('NOMBRE');
-var telefono = document.getElementById('TELÉFONO');
-var error = document.getElementById('error');
-error.style.color = 'red';
+const form = document.getElementById('form1');
+        form.addEventListener('submit', function(evt){
+            evt.preventDefault();
+            const mensajesError = [];
+            var mensajeError = [];
 
-function Mymensaje(){
-    console.log('Contratando...');
+            if(nombre.value===null||nombre.value===''){
+                mensajeError.push('Ingrese su nombre');
+            }
+            if(telefono.value===null||telefono.value===''){
+                mensajeError.push('Ingrese su teléfono ');
+            }
+            error.innerHTML = mensajeError.join(', ');
+        });
 
-    var mensajeError = [];
+const form2 = document.getElementById('form2');
+      form2.addEventListener('submit', function(evt){
+            evt.preventDefault();
+            const mensajesError = [];
+            var mensajeError = [];
 
-    if(nombre.value===null||nombre.value===''){
-        mensajeError.push('Ingrese su nombre');
-    }
-    if(telefono.value===null||telefono.value===''){
-        mensajeError.push('Ingrese su teléfono ');
-    }
-    error.innerHTML = mensajeError.join(', ');
-
-    return false;
-}
+            if(nombre.value===null||nombre.value===''){
+                mensajeError.push('Ingrese su nombre');
+            }
+            if(telefono.value===null||telefono.value===''){
+                mensajeError.push('Ingrese su teléfono ');
+            }
+            error.innerHTML = mensajeError.join(', ');
+        });
